9f691d7fe151f8e7d9163a92b1dc3489346fba02
4ev3r_0 <j.r.burks09@gmail.com>
2026-01-11 23:09:48 -0600

    setup analyzer.lua

---
+++ src/analyzer.lua
@@ -0,0 +1,11 @@
+local diff_parser = require("src.diff")
+local patterns = require("src.patterns")
+local analyzer = {}
+
+local function match_any(line,list)
+end
+
+function analizer.analyze(diff)
+end
+
+return analyzer
