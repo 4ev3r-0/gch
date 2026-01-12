b362b64eae70be4b8881569ffeb853f57af4099c
4ev3r_0 <j.r.burks09@gmail.com>
2026-01-11 23:07:55 -0600

    finished diff.lua (hopefully)

--- src/diff.lua
+++ src/diff.lua
@@ -1,6 +1,21 @@
-local diff = {}
+local d = {}
 
-function diff.parse(diff)
+function d.parse(diff)
+	local files = {}
+	local current
+
+	for line in diff:gmatch("[^\r\n]+") do
+		local file = line:match("^%+%+%+ b/(.+)")
+		if file then
+			current = file
+			files[current] = files[current] or {}
+		end
+
+		if current and (line:match("^%+") or line:match("^%-")) then
+			table.insert(files[current], line)
+		end
+	end
+	return files
 end
 
-return diff
+return d
